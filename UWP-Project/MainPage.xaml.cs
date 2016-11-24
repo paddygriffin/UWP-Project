97abe14283389b9147e521f2baf03c82ed5cba93
paddygriffin <paddygriffin1995@gmail.com>
2016-11-24 18:10:32 +0000

    added hideStatusBar, need to fix progress bar and geolocators

--- UWP-Project/MainPage.xaml.cs
+++ UWP-Project/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,23 @@ namespace UWP_Project
         public MainPage()
         {
             this.InitializeComponent();
+            HideStatusBar();
+        }
+
+        private async void HideStatusBar()
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.Phone.PhoneContract", 1, 0))
+            {
+                var statusBar = StatusBar.GetForCurrentView();
+                await statusBar.HideAsync();
+            }
+        }
+
+        private void weatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            progressBar.IsIndeterminate = true;
+            //add geo locators
+            //fix progress bar from start up page
         }
     }
 }
