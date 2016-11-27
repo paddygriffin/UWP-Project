a52d46f245b8148ce867d6790ea9801bd5984a9d
paddygriffin <paddygriffin1995@gmail.com>
2016-11-27 00:30:41 +0000

    Turned location on

--- UWP-Project/Common/APIclass.cs
+++ UWP-Project/Common/APIclass.cs
@@ -9,8 +9,8 @@ namespace UWP_Project.Common
     public class APIclass //make public as we will be implementing the APL key + link 
     {
         //API Link and Key 
-        public static string Link = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}";//might have to fix
-        public static string Key = "5cd8eff4bbf4bb6f393a6b8c101a45d2"; //allows people to access without giving out the password
+        public static string Link = "http://api.openweathermap.org/data/2.5/weather?";//might have to fix
+        public static string Key = "5cd8eff4bbf4bb6f393a6b8c101a45d2"; //allows people to access without giving out the password  lat={lat}&lon={lon}
 
         public static string APIAccess(string latitude, string longitude)
         {
--- UWP-Project/Model/WeatherAttributes.cs
+++ UWP-Project/Model/WeatherAttributes.cs
@@ -24,7 +24,7 @@ namespace UWP_Project.Model
     {
         public double temp { get; set; }
         public double pressure { get; set; }
-        public double humidity { get; set; }
+        public int humidity { get; set; }
         public double temp_min { get; set; }
         public double temp_max { get; set; }
         public double sea_level { get; set; }
